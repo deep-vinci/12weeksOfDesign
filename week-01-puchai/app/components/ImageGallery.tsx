0af04c9d5f001c41103623f5d2c0c2d9cf2692f1
Deepak <deepakworkpc@gmail.com>
2025-07-09 00:09:06 +0530

    feat: add gallery

---
+++ week-01-puchai/app/components/ImageGallery.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+
+export default function ImageGallery() {
+    return (
+        <div className="h-100 w-full">
+            <div className="grid h-100 grid-cols-5 gap-10">
+                <div className="relative h-full w-full rounded-3xl">
+                    <div
+                        // src={"/gallery1.jpg"}
+                        // width="0"
+                        // height="0"
+                        // sizes="100vw"
+                        className="h-full w-full rounded-3xl bg-[#4f7d48] bg-[url(/gallery1.jpg)] bg-cover bg-center"
+                        // alt="image"
+                    ></div>
+                </div>
+                <div className="flex items-end">
+                    <div
+                        // src={"/gallery1.jpg"}
+                        // width="0"
+                        // height="0"
+                        // sizes="100vw"
+                        className="h-[80%] w-full rounded-3xl bg-[#4f7d48]"
+                        // alt="image"
+                    ></div>
+                </div>
+                <div className="flex items-end">
+                    <div
+                        // src={"/gallery1.jpg"}
+                        // width="0"
+                        // height="0"
+                        // sizes="100vw"
+                        className="h-[60%] w-full rounded-3xl bg-[#212a20]"
+                        // alt="image"
+                    ></div>
+                </div>
+                <div className="flex items-end">
+                    <div
+                        // src={"/gallery1.jpg"}
+                        // width="0"
+                        // height="0"
+                        // sizes="100vw"
+                        className="h-[80%] w-full rounded-3xl bg-[#212a20]"
+                        // alt="image"
+                    ></div>
+                </div>
+                <div className="relative h-full w-full rounded-3xl">
+                    <div
+                        // src={"/gallery1.jpg"}
+                        // width="0"
+                        // height="0"
+                        // sizes="100vw"
+                        className="h-full w-full rounded-3xl bg-[#4f7d48]"
+                        // alt="image"
+                    ></div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- week-01-puchai/app/page.tsx
+++ week-01-puchai/app/page.tsx
@@ -1,13 +1,15 @@
 import Image from "next/image";
-import Nav from "@/app/components/nav";
+import Nav from "@/app/components/Nav";
 import Hero from "@/app/components/Hero";
+import ImageGallery from "./components/ImageGallery";
 
 export default function Home() {
     return (
-        <div className="h-[100vh] w-[100vw] bg-[#f0fffb]">
+        <div className="box-content h-[100vh] w-full bg-[#f0fffb]">
             <div className="mx-auto w-screen max-w-[1300px]">
                 <Nav />
                 <Hero />
+                <ImageGallery />
             </div>
         </div>
     );
