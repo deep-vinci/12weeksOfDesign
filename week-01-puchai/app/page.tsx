839ccc5818fac00fca9504cdb7baa10f8b5b04de
Deepak <deepakworkpc@gmail.com>
2025-07-08 22:22:38 +0530

    feat: add hero

--- week-01-puchai/app/page.tsx
+++ week-01-puchai/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import Nav from "@/app/components/nav";
+import Hero from "@/app/components/Hero";
 
 export default function Home() {
     return (
-        <div className="bg-[#f0fffb] w-[100vw] h-[100vh]">
-            <div className="w-screen max-w-[1300px] mx-auto">
+        <div className="h-[100vh] w-[100vw] bg-[#f0fffb]">
+            <div className="mx-auto w-screen max-w-[1300px]">
                 <Nav />
+                <Hero />
             </div>
         </div>
     );
